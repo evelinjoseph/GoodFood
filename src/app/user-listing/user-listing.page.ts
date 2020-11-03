b0a0cfdef7d84120416bb36849b705b085da8cb5
evelinjoseph <35345955+evelinjoseph@users.noreply.github.com>
2020-11-02 22:51:45 -0600

    listing page

--- src/app/user-listing/user-listing.page.ts
+++ src/app/user-listing/user-listing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,13 +8,48 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-listing.page.scss'],
 })
 export class UserListingPage implements OnInit {
-  item;
+  ID;
+  name;
+  description;
+  location;
+  price;
+  quantity;
+  retailerType;
+  retailerUID;
+  retailer;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private firestore: AngularFirestore) { }
 
-  ngOnInit() {
-    this.item = this.activatedRoute.snapshot.queryParamMap.get('name'); 
-    //collection.doc.id  ??
+  async ngOnInit() {
+    try{
+    this.ID = this.activatedRoute.snapshot.queryParamMap.get('id'); 
+    var self = this;
+    //collection.doc.id  ??to add to db when we get to it
+
+    var listingRef = (await this.firestore.collection("listings").doc(this.ID).get().toPromise()).data()      
+        this.name = listingRef.name;
+        this.description = listingRef.description;
+        this.location = listingRef.location;
+        this.price = listingRef.price;
+        this.quantity = listingRef.quantity;
+        this.retailerType = listingRef.retailerType;
+        this.retailerUID = listingRef.retailerUID;    
+
+    var retailerRef = this.firestore.doc(`users/${this.retailerUID}`);    
+      retailerRef.get().toPromise().then(function(doc) {        
+          if (doc.exists) {               
+               self.retailer = doc.data().name;
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+          }
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });  
   }
-
+  catch(error){
+    console.log(error.message)
+  }
 }
+}
+
