0498830258063d252ad55d08b6b50bd73d1c17f5
Thomas <tvarghese@mail.stmarytx.edu>
2020-11-05 11:45:36 -0600

    retailer profile page

--- src/app/retailertab2/retailertab2.page.ts
+++ src/app/retailertab2/retailertab2.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { UserService } from '../user.service';
+import { AlertController, NavController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-retailertab2',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Retailertab2Page implements OnInit {
 
-  constructor() { }
+  public listings: any[];
+  public listingsBackup: any[];
+  public retailers: any[];
+  public retailersBackup: any[];
 
-  ngOnInit() {
+  constructor(private nacCtrl: NavController, public afAuth: AngularFireAuth, public user: UserService, private firestore: AngularFirestore, private changeDetection: ChangeDetectorRef, public alertController: AlertController) {}
+
+  async logout() {
+    this.afAuth.signOut();
+    this.nacCtrl.navigateRoot(['./login'])
+
   }
 
+  async ngOnInit() {
+    this.listings = await this.initializeItems();  
+  }
+
+  async initializeItems(): Promise<any> {
+    const listing = await this.firestore.collection('listings')
+    .valueChanges().pipe(first()).toPromise();
+
+    console.log("reading db")
+    this.listingsBackup = listing;
+    return listing;
+  }
 }
--- src/app/retailertab3/retailertab3.page.ts
+++ src/app/retailertab3/retailertab3.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { UserService } from '../user.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { firestore } from 'firebase/app';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-retailertab3',
@@ -6,10 +13,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retailertab3.page.scss'],
 })
 export class Retailertab3Page implements OnInit {
+  ID;
+  quantity;
+  retailerType;
+  retailerUID;
+  retailer;
+  email;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private firestore: AngularFirestore, public user: UserService, private afStorage: AngularFireStorage, public afAuth: AngularFireAuth) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try{
+    var self = this;
+
+    var u = firebase.auth().currentUser;
+
+    //collection.doc.id  ??to add to db when we get to it
+
+    var listingRef = (await this.firestore.collection("users").doc(u.uid).get().toPromise()).data()
+        this.retailerType = listingRef.retailerType;
+        this.retailerUID = listingRef.retailerUID;    
+        this.email = listingRef.email;
+
+    var retailerRef = this.firestore.doc(`users/${this.retailerUID}`);    
+      retailerRef.get().toPromise().then(function(doc) {        
+          if (doc.exists) {               
+               self.retailer = doc.data().name;
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+          }
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });
+      
   }
-
+  catch(error){
+    console.log(error.message)
+  }
+}
 }
