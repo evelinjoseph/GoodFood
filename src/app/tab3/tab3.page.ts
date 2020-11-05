345a66b1f2c5cbc7346b97ca5ce5f356a6ddd944
evelinjoseph <35345955+evelinjoseph@users.noreply.github.com>
2020-11-05 11:25:50 -0600

    tab3 changes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
+import { UserService } from '../user.service'
 
 @Component({
   selector: 'app-tab3',
@@ -10,28 +11,47 @@ import * as firebase from 'firebase/app';
 export class Tab3Page {
   userUID;
   userItems;
+  items;
+  cart = [];
 
-  constructor(public afstore: AngularFirestore, private changeDetection: ChangeDetectorRef) { 
+  constructor(public afstore: AngularFirestore, private changeDetection: ChangeDetectorRef, private user: UserService) { 
 
        
   }
 
   async ngOnInit() {    
     var self = this    
-    firebase.auth().onAuthStateChanged(function(user) {  
+    await (firebase.auth().onAuthStateChanged(function(user) {  
       
       if (user) {        
         self.userUID = user.uid
-        const items = self.afstore.doc(`users/${self.userUID}`);
-        self.userItems = items.valueChanges(); 
-        self.changeDetection.detectChanges();       
+        self.items = self.afstore.doc(`users/${self.userUID}`);
+        self.userItems = self.items.valueChanges(); 
+        self.changeDetection.detectChanges();   
+        self.variables(self.userItems);    
       }
       else{
 
         console.log('no user signed in');
       }
-    });  
-       
+    }));  
+           
+  }
+
+  variables(userItems1){
+    var self = this;
+
+    this.afstore.doc(`users/${this.userUID}`).get().toPromise().then(function(querySnapshot) {
+      var cart1 = querySnapshot.get("cart");
+      cart1.forEach(element => {
+        self.cart.push(element.listingID);        
+      });
+      
+      
+  })
+  .catch(function(error) {
+      console.log("Error getting documents: ", error);
+  });
   }
 
 }
