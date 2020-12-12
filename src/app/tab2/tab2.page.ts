1e738b842882f1a450247a9537baa6a1f84deb28
evelinjoseph <35345955+evelinjoseph@users.noreply.github.com>
2020-12-11 18:04:38 -0600

    added openstreetmaps

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+//import { Map, tileLayer, marker, icon } from 'leaflet';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-tab2',
@@ -6,9 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  map: Leaflet.Map;
 
-  constructor() {}
+  constructor(public plt: Platform) {}
 
-  
+  ngOnInit() {
+    
+  }
 
+   ionViewDidEnter(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    this.map = Leaflet.map('mapId').setView([29.42412, -98.49363], 10);
+
+   
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(this.map);
+
+    
+
+       
+  }
+
+  ngOnDestroy() {
+    this.map.remove();
+  }
 }
