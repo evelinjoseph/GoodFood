187678ce16ae6766f2f87f62e755d1887c8a49d3
evelinjoseph <35345955+evelinjoseph@users.noreply.github.com>
2020-12-23 15:35:39 -0600

    restaurant mappings

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -17,17 +17,22 @@ export class Tab2Page implements OnInit{
   locationMarker;
   marker: Leaflet.marker;
   restaurants = [];
-  mapResult = [];
+  mapResult;
 
   constructor(public plt: Platform, private firestore: AngularFirestore, public changeDetection: ChangeDetectorRef, private nativeGeocoder: NativeGeocoder) {}
 
-  ngOnInit() {
-    this.getRestaurants(); 
+  async ngOnInit() {
+    await this.getRestaurants();
+    this.changeDetection.detectChanges();
+    if (this.map != undefined || this.map != null) { this.map.remove(); }
+    if(this.restaurants){
+      this.loadMap();
+    }  
   }
 
-  ionViewDidEnter(){
+  async ionViewWillEnter(){
     
-    this.getRestaurants();
+    await this.getRestaurants();
     this.changeDetection.detectChanges();
     if (this.map != undefined || this.map != null) { this.map.remove(); }
     if(this.restaurants){
@@ -52,43 +57,52 @@ export class Tab2Page implements OnInit{
       maxResults: 5
   };
 
-    this.map = Leaflet.map('mapId', {
-      zoomDelta: 0.25,
-      zoomSnap: 0
-  }).setView([29.42412, -98.49363], 10);   
+    this.map = Leaflet.map('mapId').setView([29.42412, -98.49363], 10);   
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       
     }).addTo(this.map); 
 
-    this.map.locate({ setView: true }).on("locationfound", (event: any) => { 
+  //   this.map.locate({ setView: true }).on("locationfound", (event: any) => { 
       
-      this.locationMarker = Leaflet.marker([event.latitude, event.longitude], {
-          draggable: false
-      }).addTo(this.map);
+  //     this.locationMarker = Leaflet.marker([event.latitude, event.longitude], {
+  //         draggable: false
+  //     }).addTo(this.map);
   
-      this.locationMarker.bindPopup("You are here!").openPopup();      
-  });
+  //     this.locationMarker.bindPopup("You are here!").openPopup();      
+  // });
+
+  // this.restaurants.forEach((restaurant) => {
+  //   console.log(restaurant.location)
+  //   this.nativeGeocoder.forwardGeocode(restaurant.location, options)
+  // .then((result: NativeGeocoderResult[]) => this.mapResult.push(result[0]))  
+  // .catch((error: any) => console.log(error));
+  // console.log(this.mapResult)
+
+  // this.mapResult.forEach((result) => {
+  //   Leaflet.marker([result.latitude, result.longitude], {draggable: false})
+  //   .bindPopup(`<b>${restaurant.name}</b>`, { autoClose: false })
+  //   .on('click', () => console.log(restaurant.name))
+  //   .addTo(this.map).openPopup();
+  // }); 
+  // });
+  // this.mapResult = []
+  this.changeDetection.detectChanges();
 
   console.log(this.restaurants)
+
   this.restaurants.forEach((restaurant) => {
     console.log(restaurant.location)
     this.nativeGeocoder.forwardGeocode(restaurant.location, options)
-  .then((result: NativeGeocoderResult[]) => this.mapResult = result)  
-  .catch((error: any) => console.log(error));
-  console.log(this.mapResult)
-
-  this.mapResult.forEach((result) => {
-    Leaflet.marker([result.latitude, result.longitude], {draggable: false})
-    .bindPopup(`<b>${restaurant.name}</b>`, { autoClose: false })
-    .on('click', () => console.log(restaurant.name))
-    .addTo(this.map).openPopup();
-  });   
-
-
+  .then((result: NativeGeocoderResult[]) => { 
+      Leaflet.marker([result[0].latitude, result[0].longitude], {draggable: false})
+      .bindPopup(`<b>${restaurant.name}</b>`, { autoClose: false })
+      .on('click', () => console.log(restaurant.name))
+      .addTo(this.map).openPopup();
+    }).catch((error: any) => console.log(error));       
   });
 
-
+  this.changeDetection.detectChanges();
    
   }  
 
